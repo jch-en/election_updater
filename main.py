83a83235a9f9b653c90876d24c7a923f07aac777
aeudd <106937644+aeudd@users.noreply.github.com>
2022-11-23 01:18:27 -0800

    comment from automated python script

--- main.py
+++ main.py
@@ -271,7 +271,7 @@ def git_push():
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
         origin.push()
-    except:
+    except Exception:
         print('Some error occured while pushing the code')
 
 
